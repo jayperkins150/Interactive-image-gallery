addfe6fd8031a84b8e187a348426990c6995894b
jayperkins150 <jayperkins150@yahoo.co.uk>
2025-10-31 19:10:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function addImage = {
+    const addImage = document.getElementById("imageFrame").value;
+    if ("imageInput") {
+        const gallery = document.getElementById("gallery");
+
+        // Create new gallery input div
+        const galleryItem = document.createElement("div");
+        galleryItem.classList.add("gallery-image");
+
+        // Create new image for the gallery and assign it a source
+        const img = document.createElement("img");
+        img.src = imageFrame;
+
+        // Create remove image button
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove image";
+        removeButton.classList = "remove-image";
+        removeButton.onclick = () => gallery.removeChild(galleryItem);
+
+        // Append the gallery image and remove image button to the gallery item
+        galleryItem.appendChild(img);
+        galleryItem.appendChild(removeButton);
+
+        // Append the gallery item to the gallery
+        gallery.appendChild(galleryItem);
+
+        // Clear the input field
+        document.getElementById("imageInput".value = "";)
+
+    } else {
+        alert("Please enter a valid image URL");
+    }
+
+    // Add event listener to the image input button
+    document.getElementById("addImageButton").addEventListener("click", addImage);
+}
